6cb6979e0fb33890b8a26b947c9e6546db8a56a7
peterantonarosjr <antonaros.peter.227@gmail.com>
2022-07-10 18:54:17 -0400

    import pygame/game window/game loop

--- AttackOfVampirePizzas/main.py
+++ AttackOfVampirePizzas/main.py
@@ -1,1 +1,28 @@
-#Main Python Game File
+# Importing pygame and using all functions available to us
+import pygame
+from pygame import *
+
+#Initialize pygame
+pygame.init()
+
+#Create window
+GAME_WINDOW = display.set_mode((900,400))
+display.set_caption('Attack of the Vampire Pizzas!')
+
+#Main Game Loop
+game_running = True
+
+while game_running:
+
+    #Get all events (things a user is doing in the pygame window)
+    for event in pygame.event.get():
+
+        #If the event type is QUIT then we stop the loop and exit
+        if event.type == QUIT:
+            game_running = False
+
+        #Always update the display
+        display.update()
+
+#Quit and close the window
+pygame.quit()
