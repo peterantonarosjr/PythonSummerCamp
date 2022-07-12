b260d81b4c39ee0194df2ac6b937fcd8c29b4b46
peterantonarosjr <antonaros.peter.227@gmail.com>
2022-07-11 23:05:46 -0400

    code update

--- AttackOfVampirePizzas/main.py
+++ AttackOfVampirePizzas/main.py
@@ -5,11 +5,38 @@ from pygame import *
 #Initialize pygame
 pygame.init()
 
-#Create window
-GAME_WINDOW = display.set_mode((900,400))
+#Constant variables----------------------------------------
+WINDOW_WIDTH = 1100
+WINDOW_HEIGHT = 600
+WINDOW_RES = (WINDOW_WIDTH,WINDOW_HEIGHT)
+
+#Create window---------------------------------------------
+GAME_WINDOW = display.set_mode(WINDOW_RES)
 display.set_caption('Attack of the Vampire Pizzas!')
 
-#Main Game Loop
+#Load Assets-----------------------------------------------
+#Setup the background
+bg_img = image.load('gameassets/restaurant.jpg')
+bg_surf = Surface.convert_alpha(bg_img)
+BACKGROUND = transform.scale(bg_surf,WINDOW_RES)
+
+#Setup the vampire pizza
+pizza_img = image.load('gameassets/vampire.png')
+pizza_surf = Surface.convert_alpha(pizza_img)
+VAMPIRE_PIZZA = transform.scale(pizza_surf,(100,100))
+
+#Display Assets--------------------------------------------
+#Display the background to the screen
+GAME_WINDOW.blit(BACKGROUND,(0,0))
+
+#Display the vampire pizza to the screen
+GAME_WINDOW.blit(VAMPIRE_PIZZA,(150,150))
+
+#Test drawing shapes to the screen-------------------------
+draw.circle(GAME_WINDOW,(255,0,0),(925,425),25,0)
+draw.rect(GAME_WINDOW,(0,0,255),(25,25,50,25),0)
+
+#Main Game Loop---------------------------------------------------
 game_running = True
 
 while game_running:
@@ -23,6 +50,6 @@ while game_running:
 
         #Always update the display
         display.update()
-
+#-----------------------------------------------------------------
 #Quit and close the window
 pygame.quit()
